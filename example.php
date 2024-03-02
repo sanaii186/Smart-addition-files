97654f8a0a95395951d3c13eabd346fdbfe526e2
sanaii186 <136226511+sanaii186@users.noreply.github.com>
2024-03-02 23:12:25 +0330

    Update example.php

--- example.php
+++ example.php
@@ -1,4 +1,4 @@
 <?php
 include "uix.php"; //add library
-//uix_simple_add_file("test.js","test.php","ss","test.html","test.css");
-//uix_getcontent_add_file("test.js","test.php","--><script>","test.html","test.css");
+//uix_simple_add_file("test.js","test.php","fake-file-fake--><Dkjrjs!!","test.html","test.css");
+//uix_getcontent_add_file("test.js","test.php","fake-file-fake--><Dkjrjs!!","test.html","test.css");
