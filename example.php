e7b1b0db38aa187460dc5a488a0c670f191ccf9a
sanaii186 <136226511+sanaii186@users.noreply.github.com>
2024-03-02 23:06:29 +0330

    Create example.php
    simple

---
+++ example.php
@@ -0,0 +1,4 @@
+<?php
+include "uix.php"; //add library
+//uix_simple_add_file("test.js","test.php","ss","test.html","test.css");
+//uix_getcontent_add_file("test.js","test.php","--><script>","test.html","test.css");
